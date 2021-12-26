2a8932f2a1a7f690fa37b3c454c88d5de5bef8c5
Yan <regoner2@gmail.com>
2021-12-26 05:25:03 +0200

    implemented MinHeap

--- src/Algorithms/Minheap.py
+++ src/Algorithms/Minheap.py
@@ -12,28 +12,67 @@ class MinHeap:
 
     def size(self) -> int:
         return self.heap.__len__()
+
     def swim(self, indexOfNode):
         if(indexOfNode == 1):
             return
-        parent = (int) (indexOfNode/2)
+        parentIndex = (int) (indexOfNode/2)
+        if self.heap[parentIndex] > self.heap[indexOfNode]:
+            self.swap(indexOfNode, parentIndex)
+            self.swim(parentIndex)
 
-    def sink(self):
-        pass
+    def sink(self, parent):
+        if(parent >= self.size()):
+            return
+        leftChild = parent * 2
+        rightChild = parent * 2 + 1
+        if leftChild >= self.size():
+            return
+        # parent bigger than left child and right not exist, swap and call sink
+        elif rightChild >= self.size():
+            if self.heap[leftChild] < self.heap[parent]:
+                self.swap(leftChild, parent)
+                self.sink(leftChild)
+        # parent bigger than the minimal of right and left, swap them and call recursivly
+        else:
+            minimumChild = leftChild if self.heap[leftChild] < self.heap[rightChild] else rightChild
+            if self.heap[minimumChild] < self.heap[parent]:
+                self.swap(minimumChild,parent)
+                self.sink(minimumChild)
+
+
     def insert(self, node):
         if(node == None):
             raise RuntimeWarning("Cant add null to heap")
         this.heap.append(node)
         self.keyToIndex[node.id] = self.size()-1
         self.swim(self.size()-1)
+
     def removeMin(self) -> Node:
-        pass
-    def remove(self) -> Node:
-        pass
+        self.swap(1,self.size()-1)
+        minimalNode = self.heap.pop(self.size()-1)
+        self.sink(1)
+        return minimalNode
+
+    def remove(self, index) -> Node:
+        self.swap(index,self.size()-1)
+        resNode = self.heap.pop(self.size-1)
+        self.sink(index)
+        return resNode
+
     def isEmpty(self):
         return (self.size() is 1)
-    def DecreaseKey(self):
-        pass
+
+    def DecreaseKey(self, NodeId, weight):
+        nodeIndex = self.keyToIndex.get(NodeId)
+        if weight < self.heap[nodeIndex]:
+            self.heap[nodeIndex] = weight
+            self.swim(nodeIndex)
+
     def swap(self, iIndex, jIndex):
         # swaps the indexes respectively to their location
         self.heap[iIndex], self.heap[jIndex] = self.heap[jIndex], self.heap[iIndex]
-
+        # O(n) lol
+        keyValList = list(self.keyToIndex)
+        keyValList[iIndex], keyValList[jIndex] = keyValList[jIndex], keyValList[iIndex]
+        self.keyToIndex = dict(keyValList)
